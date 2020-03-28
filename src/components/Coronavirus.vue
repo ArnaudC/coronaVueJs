52670ba5c6447e9f9c11fc89420f255a187d2b6d
Arnaud <couchet.arnaud@gmail.com>
2020-03-28 21:36:56 +0100

    View > 1000 countries.

--- src/components/Coronavirus.vue
+++ src/components/Coronavirus.vue
@@ -4,17 +4,19 @@
             <div v-if="this.latest.deathRatio !== undefined" class="coronavirus-deathratio">
                 Death ratio : {{deathRatioToFix}} %
             </div>
+            <div>Total : {{this.latest.total}}</div>
             <div>Confirmed : {{this.latest.confirmed}}</div>
             <div>Deaths : {{this.latest.deaths}}</div>
             <div>Recovered : {{this.latest.recovered}}</div>
         </div>
 
         <div class="coronavirus-holder" v-for="(l, index) in this.locations" :key="index">
-            <h1 class="coronavirus-country">{{l.country}}</h1>
+            <h1 class="coronavirus-country">{{l.country}}{{l.province ? ' - ' + l.province : ''}}</h1>
                 <div class="coronavirus-latest" v-if="l !== undefined">
                 <div class="coronavirus-deathratio">
                     Death ratio : {{l.latest.deathRatioFixed}} %
                 </div>
+                <div>Total : {{l.latest.total}}</div>
                 <div>Confirmed : {{l.latest.confirmed}}</div>
                 <div>Deaths : {{l.latest.deaths}}</div>
                 <div>Recovered : {{l.latest.recovered}}</div>
@@ -47,6 +49,7 @@ export default {
             .then(data => {
                 self.corodata = data;
                 self.latest = data?.latest ?? [];
+                this.latest['total'] = (self.latest.confirmed || 0) + (self.latest.recovered || 0) + (self.latest.deaths || 0);
                 let locationsOri = data?.locations ?? [];
                 let res = this.getLocationsWithDeathRatio(locationsOri);
                 self.locations = res;
@@ -55,10 +58,11 @@ export default {
         },
         getLocationsWithDeathRatio(data) {
             let res = data
-                .map(val => {
-                    let latestToUpdate = val.latest;
-                    val.latest.deathRatio = (latestToUpdate.deaths || 0) / ((latestToUpdate.confirmed || 0) + (latestToUpdate.recovered || 0) + (latestToUpdate.deaths || 0)) * 100;
-                    return val;
+                .map(cor => {
+                    let latestToUpdate = cor.latest;
+                    cor.latest.total = (latestToUpdate.confirmed || 0) + (latestToUpdate.recovered || 0) + (latestToUpdate.deaths || 0);
+                    cor.latest.deathRatio = (latestToUpdate.deaths || 0) / cor.latest.total * 100;
+                    return cor;
                 })
                 .sort((a, b) => { // High ratio first
                     let r1 = a.latest.deathRatio || 0, r2 = b.latest.deathRatio || 0;
@@ -68,11 +72,13 @@ export default {
                         return 1;
                     return 0;
                 })
-                .map(val => {
-                    let deathRatio = val.latest.deathRatio;
-                    val.latest.deathRatioFixed = (deathRatio === undefined || typeof deathRatio !== 'number') ? '' : deathRatio.toFixed(2);
-                    return val;
-                });
+                .map(cor => {
+                    let deathRatio = cor.latest.deathRatio;
+                    cor.latest.deathRatioFixed = (deathRatio === undefined || typeof deathRatio !== 'number') ? '' : deathRatio.toFixed(2);
+                    return cor;
+                })
+                .filter(cor => cor.latest.total >= 1000)
+            ;
             return res;
         },
     }
@@ -80,5 +86,8 @@ export default {
 </script>
 
 <style>
-
+.coronavirus-deathratio {
+    font-size: 20px;
+    margin-bottom: 7px;
+}
 </style>
